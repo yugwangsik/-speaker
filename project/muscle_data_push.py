d9d92e2114d35987e1457cc1f8f9bc201ff73e39
yugwangsik <gwangsik72@gmail.com>
2022-07-11 18:43:23 +0900

    add

--- project/muscle_data_push.py
+++ project/muscle_data_push.py
@@ -1,7 +1,7 @@
 import asyncio
 import struct
 import time
-from bleak import BleakClient
+from bleak import BleakClient, BleakScanner
 import sys
 sys.path.append("./flask/views")
 import requests
@@ -10,9 +10,12 @@ import requests
 #address = "E4:27:42:CA:AA:E5"
 #address = "C2:6B:78:BB:76:90"
 #address = "CC:AE:7F:D3:7D:08"
-#read_data = "6e400003-b5a3-f393-e0a9-e50e24dcca9e"
+read_data = "6e400003-b5a3-f393-e0a9-e50e24dcca9e"
+read_RX = "6e400002-b5a3-f393-e0a9-e50e24dcca9e"
+
 device_list = []
 address = ""
+
 
 def notify_callback(sender: int, data: bytearray):
     cnt = len(data)
@@ -24,9 +27,10 @@ def notify_callback(sender: int, data: bytearray):
     print(t)
     print('===============================================================================================')
     plus_data = t
-    #url = "http://hangyu.pe.kr:9876/auth_m/keyword"
-    #datas = {'plus':plus_data}
-    #requests.post(url, json=datas)
+    url = "http://hangyu.pe.kr:9876/auth_m/keyword"
+    datas = {'plus':plus_data}
+    requests.post(url, json=datas)
+
 
 #async def run(address, uuid, status):
 async def run(address):
@@ -35,12 +39,43 @@ async def run(address):
         services = await client.get_services()
         for service in services:
             for characteristic in service.characteristics:
+                if 'notify' in characteristic.properties:
+                    while True:
+                        url = "http://hangyu.pe.kr:9876/auth_m/send_mod"
+                        void = {'void':" "}
+                        mod = requests.post(url, json=void)
+                        mod = mod.json()
+                        mod = mod["data"]
+                        #if mod == "+" or mod == "-":
+                            #sign = mod
+                        #stat = mod.decode('utf-8')
+                        #print(mod)
+                        if mod == "1":
+                            await client.write_gatt_char(read_RX, bytes(b'1'))
+                        elif mod == "2":
+                            await client.write_gatt_char(read_RX, bytes(b'2'))
+                        elif mod == "+":
+                            
+                            await client.write_gatt_char(read_RX, bytes(b'+'))
+                        elif mod == "-":
+                            await client.write_gatt_char(read_RX, bytes(b'-'))
+
+                        await client.start_notify(characteristic, notify_callback)
+                #if 'write' in characteristic.properties:
+                    #await client.write_gatt_char(read_RX, bytes(b'1'))
+                    #await client.write_gatt_char(read_RX, bytes(b'+'))
+                    #print("+++")
+                    #num = 1
+                #if num == 1:
+                    #if 'notify' in characteristic.properties:
+                        #while True:
+                            #await client.start_notify(characteristic, notify_callback)
                 #if characteristic.uuid == uuid:
-                if characteristic.uuid == read_data:
-                    if 'notify' in characteristic.properties:
+                #if characteristic.uuid == read_data:
+                    #if 'notify' in characteristic.properties:
                         #while status == True:
-                        while True:
-                            await client.start_notify(characteristic, notify_callback)
+                        #while True:
+                            #await client.start_notify(characteristic, notify_callback)
 
 
 
