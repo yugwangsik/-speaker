291ef996b766a97f9c41c4813eb5fb4c3c20969d
yugwangsik <gwangsik72@gmail.com>
2022-08-04 13:26:48 +0900

    add

--- project/flask/views/index.py
+++ project/flask/views/index.py
@@ -1,14 +1,24 @@
 from flask.templating import render_template
 from flask import Blueprint, url_for, render_template, flash, request, session, g, jsonify
 import os
-import timer
+import subprocess
+#import timer_test
+import time
+
 
 
 bp = Blueprint('index', __name__, url_prefix='/index')
 
 val = "0"       # 센서 서버 전송 판단 변수( 0이면 전송X, 1이면 전송O )
 
-
+def logging_time(original_fn):
+    def wrapper_fn(*args, **kwargs):
+        start_time = time.time()
+        result = original_fn(*args, **kwargs)
+        end_time = time.time()
+        print("WorkingTime[{}]: {} sec".format(original_fn.__name__, end_time-start_time))
+        return result
+    return wrapper_fn
 
 @bp.route("/test", methods=["GET", "POST"])
 def test():
@@ -52,8 +62,9 @@ def update():
 
 
 @bp.route("/tt", methods=["GET", "POST"])   # '그만' 명령시 val값 변경
-@timer.logging_time
+@logging_time
 def tt():
     print("Check")
+    #subprocess.run(['python3 /home/pi/speaker_project/project/url/test.py'], shell=True)
     os.system("chromium-browser --kiosk http://34.64.199.227:9876/counter")
     return " "
---
+++ project/url/test.py
@@ -0,0 +1,4 @@
+import os
+
+
+os.system("chromium-browser --kiosk http://34.64.199.227:9876/counter")
