00030896cc5dbf7581dff58ea8eb60d676d82b3e
yugwangsik <gwangsik72@gmail.com>
2022-08-04 16:08:28 +0900

    add

--- project/flask/views/test_run.py
+++ project/flask/views/test_run.py
@@ -5,6 +5,10 @@ def check():
     ad = requests.post(url, json=datas)
     if ad == "1":
         subprocess.run(['python3 /home/pi/speaker_project/project/url/test.py'], shell=True)
+        data = "0"
+        url = "http://localhost:10001/index/tt"
+        datas = {'data':data}
+        requests.post(url json=datas)
 
 while True:
     check()
